05f870d4a2b704e4be644d0c022bd348fb2a4db0
Charlie Payne <charliepayne1101@gmail.com>
2020-04-13 09:35:29 +0000

    Automatically backed up by Learn

--- lib/find_the_minimum_value.rb
+++ lib/find_the_minimum_value.rb
@@ -1,4 +1,11 @@
 def find_min_value(array)
-  array.sort!
-  return array[0]
+  count = 0 
+  min_value = 10 
+  while array.length > count do
+    if min_value > array[count]
+      min_value = array[count]
+    end 
+    count += 1 
+  end
+  min_value
 end
